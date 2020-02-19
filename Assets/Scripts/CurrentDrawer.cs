95d4affbf0ee250222d7ff7c58419006ebc7fa4e
Vince DiCocco <vincent.dicocco@hotmail.com>
2020-02-19 09:48:18 -0500

    CurrentDrawing is working

---
+++ Assets/Scripts/CurrentDrawer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CurrentDrawer : MonoBehaviour
+{
+    [SerializeField]
+    private float timeBetweenPoints = 0.1f;
+
+    [SerializeField]
+    private CurrentScript lineRenderScript;
+
+    private float timeAtLastPoint = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Drag(BaseEventData data)
+    {
+        PointerEventData eventData = (PointerEventData)data;
+        AddPoint(eventData);
+    }
+
+    public void DragEnd(BaseEventData data)
+    {
+        PointerEventData eventData = (PointerEventData)data;
+        AddPoint(eventData);
+        lineRenderScript.ConfirmList();
+    }
+
+    private void AddPoint(PointerEventData eventData)
+    {
+        if(Time.time - timeAtLastPoint > timeBetweenPoints)
+        {
+            Vector3 pointerPos = eventData.pointerCurrentRaycast.worldPosition;
+            pointerPos.z = 0;
+            lineRenderScript.AddPoint(pointerPos);
+            timeAtLastPoint = Time.time;
+        }
+    }
+}
