3c5bf687e30f8bc4535c415c1f69d0b0cde2c035
git config --global user.name Katy-Mgit config --global user.name <kmm5278@rit.edu>
2020-03-03 19:25:47 -0500

    Added pause menu prefab

---
+++ Assets/Scripts/UIQuitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQuitGame : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void SetTimeScale(int timeScale) 
+    {
+        Time.timeScale = timeScale;
+    }
+}
