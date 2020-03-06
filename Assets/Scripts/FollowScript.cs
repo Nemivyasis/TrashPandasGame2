681cba83c09521ab7ab2ecd8f9af00a93f7136a0
Vince DiCocco <vincent.dicocco@hotmail.com>
2020-03-05 19:38:01 -0500

    Made Levels (no navigation)

---
+++ Assets/Scripts/FollowScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject followTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(followTarget != null)
+            transform.position = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+    }
+}
