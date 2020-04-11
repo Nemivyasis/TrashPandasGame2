95a3bf12b3ed141bec2a8056fe5adc593a644363
Vince DiCocco <vincent.dicocco@hotmail.com>
2020-04-11 16:31:23 -0400

    So I think I did something. Leg movement sort of works
    Could probably be cleaned up to be not fps dependent

---
+++ Assets/Scripts/LagBehindTargetMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LagBehindTargetMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    private Queue<Vector3> prevPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevPos = new Queue<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prevPos.Enqueue(target.transform.position);
+
+        Vector3 temp = Vector3.zero;
+        if (prevPos.Count >= 100)
+        {
+            temp = prevPos.Dequeue();
+        }
+        else
+        {
+            temp = prevPos.Peek();
+        }
+        transform.position = temp;
+
+    }
+}
---
+++ Assets/Scripts/PointToObj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointToObj : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.Atan2(targetObj.transform.position.y - transform.position.y, targetObj.transform.position.x - transform.position.x) + 90;
+        angle *= Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
