43e6c47aadf6509652ff1eaaf446851886d919b4
git config --global user.name Katy-Mgit config --global user.name <kmm5278@rit.edu>
2020-04-16 13:39:34 -0600

    Pop sound effect added

--- Assets/Scripts/BubbleCollectionScript.cs
+++ Assets/Scripts/BubbleCollectionScript.cs
@@ -7,11 +7,19 @@ public class BubbleCollectionScript : MonoBehaviour
 {
     int score = 0;
     public Text label;
+    public AudioClip popFX;
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Bubble")
         {
+            source.PlayOneShot(popFX);
             score += 100;
             collider.gameObject.SetActive(false);
             UpdateScore(score);
@@ -22,6 +30,7 @@ public class BubbleCollectionScript : MonoBehaviour
     {
         if (collider.gameObject.tag == "Fish")
         {
+            source.PlayOneShot(popFX);
             score += 100;
             collider.gameObject.SetActive(false);
             UpdateScore(score);
