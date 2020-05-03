fcb9b6cefdf1ba880e68c0d5d8709cad3f633632
RainyAngel <charlink22@gmail.com>
2020-05-03 15:39:19 -0400

    Added reset player prefs tool

---
+++ Assets/Scripts/EditorTools.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorTools : EditorWindow
+{
+    [MenuItem("Tools/Reset PlayerPrefs")]
+    public static void ResetPlayerPref()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("<b> **** Player Prefs Deleted **** </b>");
+    }
+}
